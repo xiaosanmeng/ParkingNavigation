2c9f9a1939b8c388355df00e5513bf12102645a9
DGMEFG <syz1729990469@163.com>
2023-10-16 23:03:24 +0800

    eveything is ok

---
+++ utils/others.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+def support_chinese():
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
+    plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
